2c60a92f08f2ae32e8ee9e9a113411e3ab2c538e
Karsten Bruckmann <karsten.bruckmann@check24.de>
2022-05-19 16:09:42 +0200

    initial commit

---
+++ apps/showcases/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kbru-os-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'showcases';
+}
